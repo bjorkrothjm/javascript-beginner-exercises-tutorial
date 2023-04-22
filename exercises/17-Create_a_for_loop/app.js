c7019354e02ec368f5d23bc7cf6280332aa9e1a2
bjorkrothjm <64375352+bjorkrothjm@users.noreply.github.com>
2023-04-22 15:10:19 +0200

    Add files via upload

--- exercises/17-Create_a_for_loop/app.js
+++ exercises/17-Create_a_for_loop/app.js
@@ -1,4 +1,12 @@
 // Declare and write your function here:
 
 
-standardsMaker();
+function standardsMaker() {
+    const phrase = "I will write questions if I'm stuck"
+    for (let i = 0; i<300; i++) {
+        console.log(phrase)
+    }
+
+}
+
+standardsMaker()
